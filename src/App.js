375f3a28d7583b1b371edfb0f9fa853f6a94a6d3
xtiantayag <xtian.tayag@gmail.com>
2020-05-31 23:14:39 +0800

    add style root
    add style root for the whole return or the app root

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
-import Radium from 'radium';
+import Radium, { StyleRoot } from 'radium';
 
 class App extends Component {
     state = {
@@ -106,6 +106,7 @@ class App extends Component {
         }
 
         return (
+            <StyleRoot>
             <div className="App" >
                 <h1> Hi, I 'm a React App</h1>
                 <p className={cssClasses.join(' ')}> This is really working! </p>
@@ -116,6 +117,7 @@ class App extends Component {
                 {persons}
 
             </div>
+            </StyleRoot>
         );
         // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
     }
